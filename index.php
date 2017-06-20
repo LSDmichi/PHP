08bd9cb2d77a5064558001f9f0da3ca6c797acf0
LSDmichi <masamichi.hiagshi@icloud.com>
2017-06-20 13:53:37 +0900

    first try

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <p>ようこそ！！まさおのAPPへ！！</p>
+  <a href="new.php">新規作成</a>
+  <tr>
+    <th>番号</th>
+    <th>LIST</th>
+    <th>更新</th>
+    <th>削除</th>
+  </tr>
+</body>
+</html>
---
+++ new.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+  <p>ここでは新規作成をしましすよ！！</p>
+  <form action="store" method="post">
+    <input type="text" name="TODO" value="">
+    <input type="submit" value="送信！！">
+  </form>
+</body>
+</html>
